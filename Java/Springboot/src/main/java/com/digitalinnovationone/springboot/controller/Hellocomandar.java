8f648cea4bc98ded1425a131af8b3c943c7eafd3
LucasTARosa <tlucas.23@gmail.com>
2021-05-12 14:12:55 -0300

    Fim da introdução ao Springboot

--- Java/Springboot/src/main/java/com/digitalinnovationone/springboot/controller/Hellocomandar.java
+++ Java/Springboot/src/main/java/com/digitalinnovationone/springboot/controller/Hellocomandar.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class Hellocomandar {
 
     @GetMapping("/")
-    public String helloMessage(){
-        return "Hello, World!";
+    public String helloMessage() {
+        return "Hello, World";
     }
 }
