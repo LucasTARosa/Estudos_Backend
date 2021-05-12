396dc4e9c126b954d828016e727dea749790fb6b
LucasTARosa <tlucas.23@gmail.com>
2021-05-12 15:49:48 -0300

    Fim da aula sobre SpringBoot

---
+++ Java/Springboot/src/main/java/com/digitalinnovationone/springboot/config/DBConfiguration.java
@@ -0,0 +1,37 @@
+package com.digitalinnovationone.springboot.config;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+@Configuration
+@ConfigurationProperties("spring.datasource")
+@Setter
+@Setter
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection(){
+        System.out.print("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to H2_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String productionDatabaseConnection(){
+        System.out.print("DB connection for Production - MYSQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_PROD - Production instance";
+    }
+
+}
