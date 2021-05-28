cca3d141e3f9912b2fcdb7243d5bf00ba794a375
LucasTARosa <tlucas.23@gmail.com>
2021-05-28 14:20:14 -0300

    Estudo de Node JS

---
+++ Java/jax-rs/src/test/java/br/com/digitalinnovationone/jaxrs/JaxRsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovationone.jaxrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
